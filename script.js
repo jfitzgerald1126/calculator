2d36fa80a80fb9bbc17810838a3ae28e8e443174
john <jfitzgerald1126@gmail.com>
2023-08-24 14:56:01 -0400

    Add operate func and glob vars for operands

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// globals
+let first;
+let second;
+let operator;
+
+// math functions
 function add (a, b) {
     return +a + +b;
 }
@@ -12,4 +18,24 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return +a / +b;
+}
+
+
+function operate(f, s, op) {
+    switch (op) {
+        case '+':
+            return add(f, s);
+            break;
+        case '-':
+            return subtract(f, s);
+            break;
+        case '*':
+            return multiply(f, s);
+            break;
+        case '/':
+            return divide(f, s);
+            break;
+        default:
+            break;
+    }
 }
